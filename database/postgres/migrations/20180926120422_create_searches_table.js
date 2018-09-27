753b9bcdd22dd11ea0f8a941723e4f418e8f0ab1
Kenneth Oh <ohtkenneth@gmail.com>
2018-09-27 14:26:20 -0700

    Optimize seed file
    Creating rows with fs.appendFileSync takes ~11 min, try using streams to reduce execution times. Writing to stream as rows are created increases time exponentially. Try holding all rows in memory to create readstream and pipe to writeStream.

--- database/postgres/migrations/20180926120422_create_searches_table.js
+++ database/postgres/migrations/20180926120422_create_searches_table.js
@@ -1,11 +1,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('searches', (table) => {
-    table.increments('id').primary();
+    table.increments('id');
     table.string('brand');
     table.string('search');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('searches');
+  return knex.schema.dropTableIfExists('searches');
 };
--- database/postgres/seed.js
+++ database/postgres/seed.js
@@ -1,11 +1,74 @@
+const path = require('path');
+const fs = require('fs');
+const perf = require('execution-time')();
 const faker = require('faker');
-const insert = require('./models').insertSearch;
 
-for (let i = 0; i <= 100000; i++) {
-  const randSearch = faker.commerce.productName();
-  const randBrand = faker.company.companyName();
+const times = 1000000;
 
-  insert(randBrand, randSearch)
-    .then(() => console.log(`inserted ${randBrand}, ${randSearch}`))
-    .catch(err => console.log(err));
+console.log('Writing data...');
+perf.start();
+const dataWriteStream = fs.createWriteStream('./data/data.tsv', {
+  flags: 'a',
+  encoding: 'utf8',
+});
+let rows = [];
+// console.log(`Starting stream ${streams + 1} of 10`);
+for (let i = 1; i <= times; i++) {
+  rows.push(`${i}\t${faker.commerce.productName()}\t${faker.company.companyName()}\n`);
+  if (i % 100000 === 0) {
+    console.log(`Writing ${i} of ${times} rows`);
+  }
+  // dataWriteStream.write(`${i}\t${faker.commerce.productName()}\t${faker.company.companyName()}\n`, 'utf8', (status) => {
+    
+  //   console.log(status)
+  // });
+  
 }
+// dataWriteStream.end();
+const results = perf.stop();
+console.log('writeData execution time: ' + (results.time / 1000).toFixed(5));  
+
+
+// console.log('Writing data...');
+// perf.start();
+// for (let streams = 0; streams < 10; streams++) {
+//   const dataWriteStream = fs.createWriteStream('./data/data.tsv', {
+//     flags: 'a',
+//     encoding: 'utf8',
+//   });
+//   console.log(`Starting stream ${streams + 1} of 10`);
+//   for (let i = 1; i <= times; i++) {
+//     dataWriteStream.write(`${i}\t${faker.commerce.productName()}\t${faker.company.companyName()}\n`, 'utf8', (status) => {
+//       console.log(status)
+//     });
+//     if (i % 100000 === 0) {
+//       console.log(`Writing ${i} of ${times} rows`);
+//     }
+//   }
+//   dataWriteStream.end();
+// }
+// const results = perf.stop();
+// console.log('writeData execution time: ' + (results.time / 1000).toFixed(5));  
+
+
+
+// function writeData() {
+  // console.log('Writing data...');
+  // perf.start();
+//   for (let i = 1; i <= times; i++) {
+//     // create sql row tab delimited columns
+//     const row = `${i}\t${faker.commerce.productName()}\t${faker.company.companyName()}\n`;
+//     try {
+//       fs.appendFileSync('./data/data.tsv', row);
+//       if (i % 1000000 === 0) {
+//         console.log(`Row ${i} of ${times} created`);
+//       }
+//     } catch(err) {
+//       console.log(err);
+//     }
+//   }
+  // const results = perf.stop();
+  // console.log('writeData execution time: ' + (results.time / 1000).toFixed(5));  
+// }
+
+// writeData();
