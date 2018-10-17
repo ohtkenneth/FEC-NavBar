796714827aac55867b2eafcc8dd8ad39ab28c122
ohtkenneth <ohtkenneth@gmail.com>
2018-10-17 16:40:42 -0700

    Random ids should be across all database to fully use shards

--- service-server/src/cluster.js
+++ service-server/src/cluster.js
@@ -5,7 +5,7 @@ const cores = require('os').cpus().length;
 // if cluster is master, take care of managing child processes
 if (cluster.isMaster) {
   // create cluster
-  for (let i = 0; i < cores; i++) {
+  for (let i = 0; i < 2; i++) {
     cluster.fork();
   }
   // whenever a cluster dies, create a replacement one
@@ -13,5 +13,5 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
-  require('./ssr.js');
+  require('./server.js');
 }
